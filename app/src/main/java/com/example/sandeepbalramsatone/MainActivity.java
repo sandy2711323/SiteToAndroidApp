982c47f883be7dc093e34b17b491a47599c9638d
sandy2711323 <sandy2711323@gmail.com>
2016-02-22 23:13:40 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/sandeepbalramsatone/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.sandeepbalramsatone;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+    ProgressDialog mProgress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient());
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mProgress = ProgressDialog.show(this, "Loading", "Please wait for a moment...");
+        mWebView.setWebViewClient(new MyAppWebViewClient(mProgress));
+
+        // Use remote resource
+         mWebView.loadUrl("http://sandeepbalramsatone.com/");
+
+        // Stop local links and redirects from opening in browser instead of WebView
+        // mWebView.setWebViewClient(new MyAppWebViewClient());
+
+        // Use local resource
+        // mWebView.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    // Prevent the back-button from closing the app
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/sandeepbalramsatone/MyAppWebViewClient.java
@@ -0,0 +1,39 @@
+package com.example.sandeepbalramsatone;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MyAppWebViewClient extends WebViewClient {
+    ProgressDialog mProgress;
+    public MyAppWebViewClient( ProgressDialog progress){
+
+        this.mProgress = progress;
+
+    }
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        if (Uri.parse(url).getHost().endsWith("sandeepbalramsatone.com")) {
+            return false;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        view.getContext().startActivity(intent);
+        return true;
+    }
+
+    // when finish loading page
+    public void onPageFinished(WebView view, String url) {
+        if (mProgress.isShowing()) {
+            mProgress.dismiss();
+        }
+    }
+
+
+}
